760b3fdd30a0ebede85dae66ce26cb6ae9770948
iam_mamadou <66375343+momodou44@users.noreply.github.com>
2024-03-05 13:13:15 +0000

    Create dockerBuild.groovy

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,10 @@
+def call(String dockerHubUsername, String imageName) {
+    // Build the Docker image
+    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=a16484f704msh1d544d09b252e05p1b1054jsn4aafc2eb9adf -t ${imageName} ."
+     // Tag the Docker image
+    sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
+    // Push the Docker image
+    withDockerRegistry([url: 'https://index.docker.io/v1/', credentialsId: 'docker']) {
+        sh "docker push ${dockerHubUsername}/${imageName}:latest"
+    }
+}
